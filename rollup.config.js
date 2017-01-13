33aae3356b9a47ea9f7e2ca0694e9127af3b5340
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-13 11:44:21 +0300

    👷🏻 Updated builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 'use strict';
 
 export default {
+	entry: './index.js',
 	plugins: [{
 		name: 'json',
 		transform(code, id) {
 			return id.slice(-5) === '.json' ? `export default ${code}` : null;
 		}
-	}]
+	}],
+	targets: [
+		{format: 'cjs', dest: 'dist/snippets.cjs.js'},
+		{format: 'es',  dest: 'dist/snippets.es.js'}
+	]
 };
